741ea247359f0b64b979d4f942529fe9300a7f91
adaoud <amine.daoud@ensta.fr>
2016-05-18 21:46:29 -0700

    2ème modification

--- process.py
+++ process.py
@@ -14,14 +14,15 @@ with open("res.txt") as res_f:
 
 result = []
 res.remove('\n')
+
 for line in res:
         
 	nb_thread, time = line[:].split(";",1)
-        
 	result.append((int(nb_thread), float(time)))
 	
 reference_time = result[0][1]
 output.write("nb_thread ; speedup ; efficiency\n")
+
 for nb_thread, time in result:
 	speed_up = time/reference_time
 	efficiency = speed_up/nb_thread
